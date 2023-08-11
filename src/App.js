bbd7cc043e0ecf03a68893e4171f9f6e96b2190a
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-08-11 14:58:06 +0700

    Create class-components.js
    Create class-components.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import './style.css'
+
+import {Header,Content,Footer}from'./func-componanents'
+
+import Caledar from './class-components';
+
+function App(){
+  return(
+    <>
+    <Header/>
+    <p><center><Caledar/></center></p>
+    <Content/>
+    <Footer/>
+    </>
+  )
+}
+/*import './style.css'
 function App() {
   let colors = ['red','green','blue','yellow']
   let list = colors.map(c=><li>{c}</li>)
 
-  return<ul>{list}</ul>
+  return<ul>{list}</ul>*/
  /* return(
     <>
     <h2>DeveloperThai.com</h2>
     <div className='title'>React&React Native</div>
     </>
-  )*/
+  )
     
-}
+}*/
 
 export default App;
