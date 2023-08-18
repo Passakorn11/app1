53bc4628f49b950b51ff5247e924a04d26a64ba9
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-08-18 13:35:39 +0700

    banner
    banner

---
+++ src/banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function randlnt(min,max){
+    let x =(max-min)+1
+    return min + Math.floor(Math.random()*x)
+}
+export default function Banner(){
+    const = randlnt(1,3)
+}
