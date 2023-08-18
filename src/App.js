04e8ef5542c25420fe35958e547a71cc38cd7eed
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-08-18 15:27:57 +0700

    event-data.js
    event-data.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,5 @@
 import React from 'react'
-import { Calculator2 } from './calculator';
-import Banner from './banner';
-/*import {Header,Content,Footer}from'./func-componanents'
-
-import Caledar from './class-components';
-
-function App(){
-  return(
-    <>
-    <Header/>
-    <p><center><Caledar/></center></p>
-    <Content/>
-    <Footer/>
-    </>
-  )
-}*/
-/*import './style.css'
-function App() {
-  let colors = ['red','green','blue','yellow']
-  let list = colors.map(c=><li>{c}</li>)
-
-  return<ul>{list}</ul>*/
- /* return(
-    <>
-    <h2>DeveloperThai.com</h2>
-    <div className='title'>React&React Native</div>
-    </>
-  )
-    
-}*/
-
+import { EventData2 } from './event-data'
 export default function App(){
-  return<Calculator2/>
+  return <EventData2/>
 }
