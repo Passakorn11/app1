1f42976193a4c1cafd016b53eeee913b893b74ec
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-08-25 15:44:44 +0700

    update
    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { EventData2 } from './event-data'
-import RefsFunc from './refs-func'
-import RefArray from './refs-array'
 import MessageBox from './state-func'
 
  export default function App(){
