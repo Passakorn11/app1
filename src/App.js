57a40a703d0ce7f7ebb1fc6f09ae0d97245133ef
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-08-25 14:36:21 +0700

    state-func.js
    state-func.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { EventData2 } from './event-data'
 import RefsFunc from './refs-func'
+import RefArray from './refs-array'
+import MessageBox from './state-func'
 
  export default function App(){
-  return <RefsFunc/>
+  return <MessageBox/>
 }
