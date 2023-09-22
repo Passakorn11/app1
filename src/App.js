06833782f9b18017b92f390d6f46f79fda7127e4
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-09-22 15:23:15 +0700

    route
    route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { userContext } from './context'
 import Header2 from './context-header2'
 import Content2 from './context-content2'
+import Router2 from "./router2"
+import Router3 from "./router3"
 
+export default function App() {
+return <Router3/>
+}
 /*import MessageBox from './state-func'
 
  export default function App(){
   return <MessageBox/>
 }*/
-export default function App(){
+/*export default function App(){
   let[user,setUser]=React.useState('')
   return(
     <userContext.Provider value={[user,setUser]}>
@@ -16,4 +21,4 @@ export default function App(){
      <Content2/>
     </userContext.Provider>
   )
-}
+}*/
