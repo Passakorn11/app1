d00c3154e5fe2db5105af5d44881a7342712b231
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-09-29 15:53:19 +0700

    formcheck
    formcheck

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import FormBootstrap from './form-bootstrap'
-import FormText from './form-text'
+import FormCheck from './form-check'
 export default function App() {
-return <FormText/>
+  return <FormCheck/>
+//return <FormText/>
 }
 /*import MessageBox from './state-func'
 
