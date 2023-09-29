52a577e84ef413ecbac30b0dc1ad9d5f1dc1815d
Passakorn11 <110074241+Passakorn11@users.noreply.github.com>
2023-09-29 14:44:27 +0700

    bootstrap
    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import { userContext } from './context'
-import Header2 from './context-header2'
-import Content2 from './context-content2'
-import Router2 from "./router2"
-import Router3 from "./router3"
-
+import FormBootstrap from './form-bootstrap'
+import FormText from './form-text'
 export default function App() {
-return <Router3/>
+return <FormText/>
 }
 /*import MessageBox from './state-func'
 
